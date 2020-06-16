cfa16ec1c3ba66128691f0a93929f71bb4f682f6
prakhar-dhakar <37078245+prakhar-dhakar@users.noreply.github.com>
2020-06-16 15:24:48 +0530

    Update extract.sh

--- extract.sh
+++ extract.sh
@@ -7,7 +7,7 @@ python names.py
 cd darknet && ./darknet detector test cfg/coco.data cfg/yolov3-spp.cfg yolov3-spp.weights -thresh 0.05 -ext_output -dont_show -out result.json < data/train.txt
 cd ..
 
-cp ../Video1/video-to-test/*.jpg ./testingimages/
+# cp ../Video1/video-to-test/*.jpg ./testingimages/
 
 
 python extract.py 
