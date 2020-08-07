5fcaeb5650ba14c4cd756ec36c6a0df5b8d60f91
prakhar-dhakar <37078245+prakhar-dhakar@users.noreply.github.com>
2020-08-07 11:42:03 +0530

    use yolov4 and update confidence threshold

--- extract.sh
+++ extract.sh
@@ -4,7 +4,7 @@ rm poses/*.jpg
 
 python names.py
 
-cd darknet && ./darknet detector test cfg/coco.data cfg/yolov3-spp.cfg yolov3-spp.weights -thresh 0.05 -ext_output -dont_show -out result.json < data/train.txt
+cd darknet && ./darknet detector test cfg/coco.data cfg/yolov4.cfg yolov4.weights -thresh 0.25 -ext_output -dont_show -out result.json < data/train.txt
 cd ..
 
 # cp ../Video1/video-to-test/*.jpg ./testingimages/
